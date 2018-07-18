d3230c36ca1384be8f9914920f126da3122419fe
Jeff Cacossa <jeff.cacossa@gmail.com>
2018-07-18 14:50:31 -0400

    Add support for Redux Devtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,27 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+
 export default function configureStore(initialState) {
-  return createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(thunk, reduxImmutableStateInvariant())
-  );
+    const composeEnhancers =
+        typeof window === 'object' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) :
+            compose
+
+    const middleware = [
+        thunk,
+        reduxImmutableStateInvariant()
+    ]
+
+    const enhancer = composeEnhancers(
+        applyMiddleware(...middleware)
+    )
+
+    return createStore(
+        rootReducer,
+        initialState,
+        enhancer
+    )
 }
